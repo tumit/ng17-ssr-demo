b729e7b94c032e2a37f3aac7bd1dde1c2f079108
tumit <tumit@live.com>
2023-12-23 18:53:28 +0700

    feat(bootstrap): init bootstrap & bootstrap-icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgIconComponent, NgIconsModule, provideIcons } from '@ng-icons/core';
+import { bootstrapCheck } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
+import { provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import { bootstrapCheck } from '@ng-icons/bootstrap-icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideIcons({ bootstrapCheck }),
+    provideNgIconsConfig({
+      size: '1.25em',
+    }),
+  ],
 };
+
